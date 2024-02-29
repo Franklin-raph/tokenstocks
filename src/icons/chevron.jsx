d3e55dbbfe758c9c031053b7c2669dbcb58cac9e
Okoro James <okorojameschizaram@gmail.com>
2024-02-29 14:28:21 +0100

    [UPDATE] Growing interest section completed, started Best Stories section

---
+++ src/icons/chevron.jsx
@@ -0,0 +1,19 @@
+export const ChevronDown = ({ className }) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="32"
+      height="32"
+      viewBox="0 0 24 24"
+      className={`stroke-dark-200 ${className}`}
+    >
+      <path
+        fill="none"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="m6 9l6 6l6-6"
+      />
+    </svg>
+  );
+};
