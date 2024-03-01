ebbdbd82b0aff3b66330e8554d47cdbeaf345a68
Okoro James <okorojameschizaram@gmail.com>
2024-03-01 12:52:25 +0100

    [UPDATE] footer completed

---
+++ src/icons/facebook.jsx
@@ -0,0 +1,14 @@
+export const FacebookIcon = ({ className }) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 512 512"
+      className={`fill-light-200 size-6 ${className}`}
+    >
+      <path
+        d="M288 192v-38.1c0-17.2 3.8-25.9 30.5-25.9H352V64h-55.9c-68.5 0-91.1 31.4-91.1 85.3V192h-45v64h45v192h83V256h56.4l7.6-64h-64z"
+        className="fill-light-200"
+      />
+    </svg>
+  );
+};
